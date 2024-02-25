2a75b3eda1459ad1d141332a4ee3f5d65f4a9cba
Oleg Skiba <107154680+OlegSkiba@users.noreply.github.com>
2024-02-25 22:56:21 +0200

    Add files via upload

---
+++ WebApplication1/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace WebApplication1
+{
+    [ApiController]
+    [Route("Library/Books")] // Встановлення шляху маршрутизації
+    public class BooksController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public BooksController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet] // Вказуємо, що цей метод відповідає на HTTP GET-запити
+        public ActionResult<IEnumerable<string>> GetBooks()
+        {
+            // Отримуємо дані про книги з файлу конфігурації
+            var books = _configuration.GetSection("Library:Books").Get<List<string>>();
+
+            if (books == null)
+            {
+                return NotFound(); // Повертаємо 404 Not Found, якщо список книг порожній або не знайдений
+            }
+
+            return Ok(books);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication1
+{
+    [ApiController]
+    [Route("Library/Profile")] // Встановлення шляху маршрутизації
+    public class ProfileController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProfileController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("{id?}")] // Вказуємо, що цей метод відповідає на HTTP GET-запити з необов'язковим параметром id
+        public ActionResult<string> GetUserProfile(int? id)
+        {
+            // Отримуємо інформацію про користувача з файлу конфігурації
+            string sectionName = id.HasValue ? $"Users:User{id}" : "Users:CurrentUser";
+            var userProfile = _configuration.GetValue<string>(sectionName);
+
+            if (userProfile == null)
+            {
+                return NotFound(); // Повертаємо 404 Not Found, якщо інформація про користувача не знайдена
+            }
+
+            return Ok(userProfile);
+        }
+    }
+}
