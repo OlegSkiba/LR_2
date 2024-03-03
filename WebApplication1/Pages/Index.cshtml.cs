06f55a197e404c9303c32b74b096356e1e0fc8c4
Oleg Skiba <107154680+OlegSkiba@users.noreply.github.com>
2024-03-03 13:57:56 +0200

    Index.cshtml.cs

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication1.Pages
 {
@@ -12,9 +14,29 @@ namespace WebApplication1.Pages
             _logger = logger;
         }
 
-        public void OnGet()
-        {
+        [BindProperty]
+        public string Value { get; set; }
 
+        [BindProperty]
+        public string Date { get; set; }
+
+        public IActionResult OnPost()
+        {
+            // Parse date from string
+            if (DateTime.TryParse(Date, out DateTime expirationDate))
+            {
+                // Set value in cookies with expiration date equal to Date
+                Response.Cookies.Append("MyValue", Value, new CookieOptions
+                {
+                    Expires = expirationDate
+                });
+                return RedirectToPage();
+            }
+            else
+            {
+                // Handle invalid date format
+                return Page();
+            }
         }
     }
 }
